4813294850c52e75c9260af5400e2c502b3270a9
Nesela <icarus_cat@naver.com>
2026-05-26 16:26:54 +0900

    닉네임 알수없음 내용 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,18 @@
 package org.example;
 
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.GatewayIntent; // 이 줄이 있어야 합니다!
 
 public class Main {
     public static void main(String[] args) {
         String token = System.getenv("DISCORD_TOKEN");
 
-        JDA jda = JDABuilder.createDefault(token)
-                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+        // 여기서 .enableIntents(...) 안에 꼭 GUILD_MEMBERS가 있어야 합니다!
+        JDABuilder.createDefault(token)
+                .enableIntents(
+                        GatewayIntent.MESSAGE_CONTENT,
+                        GatewayIntent.GUILD_MEMBERS // [핵심] 이 줄이 없어서 에러가 난 것입니다!
+                )
                 .addEventListeners(new MessageListener())
                 .build();
 
